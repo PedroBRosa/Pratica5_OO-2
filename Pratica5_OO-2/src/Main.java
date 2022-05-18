27d27444c59c7293dbf9dab38e2610265b55ff0e
Pedro Rosa <92191060+PedroBRosa@users.noreply.github.com>
2022-05-17 21:47:43 -0300

    Implementação da 39

--- Pratica5_OO-2/src/Main.java
+++ Pratica5_OO-2/src/Main.java
@@ -4,17 +4,30 @@ public class Main {
     public static void main(String[] args) {
         String nome;
         float altura, peso;
-        Pessoa[] pessoas = new Pessoa[2];
+        int idade;
+        Pessoa[] pessoas = new Pessoa[10];
         Scanner ler = new Scanner(System.in);
-        for (int i = 0; i<pessoas.length;i++){
+        for (int i = 0; i < pessoas.length; i++) {
             System.out.println("Escreva o nome: ");
             nome = ler.next();
             System.out.println("Escreva a altura: ");
             altura = ler.nextFloat();
             System.out.println("Escreva o peso: ");
             peso = ler.nextFloat();
-            pessoas[i]= new Pessoa(nome,altura,peso);
+            System.out.println("Escreva a idade: ");
+            idade = ler.nextInt();
+            pessoas[i] = new Pessoa(nome, altura, peso,idade);
         }
-        for (int i = 0; i<pessoas.length;i++) System.out.println(pessoas[i].imprimir());
+//                                COMPARANDO SE OS OBJETOS SÃO IGUAIS
+//        for (int i = 0; i < pessoas.length; i++) System.out.println(pessoas[i].imprimir());
+//        Funcionario fun1 = new Funcionario("Pedro", "TI", "Bradesco", "316547436546", 2000.00);
+//        Funcionario fun2 = new Funcionario("Maria", "TI", "Bradesco", "316547436546", 2000.00);
+//        if (fun1 == fun2) {
+//            System.out.println("Os dados são iguais");
+//        } else {
+//            System.out.println("os dados são diferentes");
+//        }
+
+
     }
 }
--- Pratica5_OO-2/src/Pessoa.java
+++ Pratica5_OO-2/src/Pessoa.java
@@ -2,10 +2,21 @@ public class Pessoa {
     private String nome;
     private float altura,peso;
 
-    public Pessoa(String nome, float altura, float peso) {
+    private int idade;
+
+    public Pessoa(String nome, float altura, float peso,int idade) {
         this.nome = nome;
         this.altura = altura;
         this.peso = peso;
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 
     public String getNome() {
@@ -32,6 +43,9 @@ public class Pessoa {
         this.peso = peso;
     }
     public String imprimir(){
-        return " Nome: "+getNome()+"\n Altura: "+getAltura()+"\n Peso: "+getPeso()+"\n ============";
+        return " Nome: "+getNome()+"\n Altura: "+getAltura()+"\n Peso: "+getPeso()+"\n Idade: "+getIdade()+"\n ============";
+    }
+    public void aniversario(){
+        idade++;
     }
 }
